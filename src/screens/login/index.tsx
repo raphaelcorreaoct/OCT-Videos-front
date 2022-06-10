8f413c2d051e6a00e391279d17198edb655fcd48
Raphael Correa <raphael.correa@bennu.com.br>
2022-06-10 18:47:21 -0300

    feat(UI):Tela de login

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import './login.css';
 
 export default function index() {
-	return <div>index</div>;
+	return (
+		<main>
+			<div className="wrapper">
+				<div className="wrapp-all">
+					<header>
+						<img src="/assets/oct-logo.svg" alt="OCT Videos" className="logo" />
+						<h2>Faça seu login e comece a criar!</h2>
+					</header>
+					<div className="card">
+						<form action="">
+							<div className="form-group">
+								<label>E-mail</label>
+								<input type="text" />
+							</div>
+							<div className="form-group">
+								<label>Senha</label>
+								<input type="password" />
+							</div>
+							<div className="form-group">
+								<button> Login </button>
+							</div>
+							<hr />
+							<footer>
+								<a href="/cadastr-se">Cadastre-se</a>
+								<a href="/cadastr-se">Eequeci minha senha</a>
+							</footer>
+						</form>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
